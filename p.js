1cce6b0693b588f374e76422f21f4ca42b32325d
Steve Geluso <stevegeluso@gmail.com>
2017-11-02 16:06:56 -0700

    140

--- p.js
+++ p.js
@@ -1,5 +1,5 @@
 p=(r="")=>{
-d=(j,f,k)=>{for(var i=0;i<j;i++){f(i)}{for(var i=k||j;i>=0;i--){f(i)}}}
+d=(j,f,k)=>{for(var i=0;i<j;i++)f(i);for(var i=k||j;i>=0;i--)f(i)}
 d(10,(l)=>{d(l,(i)=>r+=i);r+="\n"},10-1-1)
 return r.trim()
 }
